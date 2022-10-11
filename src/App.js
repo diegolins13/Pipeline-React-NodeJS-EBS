3aa903a238672f19fb0e987851d1f411b71a223a
Diego Lins <diegotec.lins@gmail.com>
2022-10-11 00:44:30 -0300

    teste 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Diego Lins!
+          Teste 02
         </p>
         <a
           className="App-link"
