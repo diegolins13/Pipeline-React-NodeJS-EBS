0b9a33b647021f756b5e06e50369cff8947426c3
Diego Lins <diegotec.lins@gmail.com>
2022-10-13 11:06:29 -0300

    teste 03

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Teste 02
+          Teste 03
         </p>
         <a
           className="App-link"
